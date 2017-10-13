7dd5d1bbb68e5bc2e45cf9a6bb5aedfdc414a292
Albert Weichselbraun <albert@weichselbraun.net>
2017-10-13 18:52:08 +0200

    fix: table unit tests
    * adjust unit tests to the new table alignment algorithm

--- tests/test_table.py
+++ tests/test_table.py
@@ -22,4 +22,4 @@ def test_table():
     t.add_text("c")
 
     text = t.get_text()
-    assert text == u'first second third \na     b      c     '
+    assert text == u'first second third\na     b      c    '
