5ba35811447890535dfb4ab967b88d6992c8f4ff
max <mcgoebel@gmail.com>
2018-12-17 15:00:39 +0100

    chg: remove encoding from file open

--- setup.py
+++ setup.py
@@ -7,37 +7,37 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README.md file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-      long_description = f.read()
+with open(path.join(here, 'README.md')) as f:  # , encoding='utf-8'
+    long_description = f.read()
 
 setup(
-      # Metadata
-      name="inscriptis",
-      version="0.0.3.5",
-      description='inscriptis - HTML to text converter.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Albert Weichselbraun, Fabian Odoni',
-      author_email='albert.weichselbraun@htwchur.ch, fabian.odoni@htwchur.ch',
-      classifiers = [
+    # Metadata
+    name="inscriptis",
+    version="0.0.3.5",
+    description='inscriptis - HTML to text converter.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Albert Weichselbraun, Fabian Odoni',
+    author_email='albert.weichselbraun@htwchur.ch, fabian.odoni@htwchur.ch',
+    classifiers=[
            'Topic :: Text Processing :: Markup :: HTML',
            'Programming Language :: Python :: 3',
-      ],
-      url='http://github.com/weblyzard/inscriptis',
-      license="GPL2",
-      package_dir={'': 'src'},
+    ],
+    url='http://github.com/weblyzard/inscriptis',
+    license="GPL2",
+    package_dir={'': 'src'},
 
-      # Package List
-      packages=find_packages('src'),
+    # Package List
+    packages=find_packages('src'),
 
-      # Scripts
-      scripts=[
+    # Scripts
+    scripts=[
         'scripts/inscript.py'
-      ],
+    ],
 
-      # Requirements
-      install_requires=[
+    # Requirements
+    install_requires=[
         'lxml'
-      ]
+    ]
 
 )
