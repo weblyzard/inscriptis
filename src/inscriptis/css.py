9a4829464f5d42213058d4e83c89772ee0297312
Albert Weichselbraun <albert@weichselbraun.net>
2018-12-21 15:24:17 +0100

    fix: handling of negative margins.

--- src/inscriptis/css.py
+++ src/inscriptis/css.py
@@ -7,13 +7,15 @@ Handeling of CSS files.
 from re import compile as re_compile
 from inscriptis.html_properties import Display, WhiteSpace
 
+
 class HtmlElement(object):
 
     __slots__ = ('tag', 'prefix', 'suffix', 'display', 'margin_before',
                  'margin_after', 'padding', 'whitespace')
 
-    def __init__(self, tag='/', prefix='', suffix='', display=Display.inline, \
-            margin_before=0, margin_after=0, padding=0, whitespace=WhiteSpace.normal):
+    def __init__(self, tag='/', prefix='', suffix='', display=Display.inline,
+                 margin_before=0, margin_after=0, padding=0,
+                 whitespace=WhiteSpace.normal):
         self.tag = tag
         self.prefix = prefix
         self.suffix = suffix
@@ -35,15 +37,16 @@ class HtmlElement(object):
     def __str__(self):
         return "<{tag} prefix={prefix}, suffix={suffix}, display={display}, " \
                "margin_before={margin_before}, margin_after={margin_after}, " \
-               "padding={padding}, whitespace={whitespace}>".format( \
-                  tag=self.tag, \
-                  prefix=self.prefix, \
-                  suffix=self.suffix, \
-                  display=self.display, \
-                  margin_before=self.margin_before, \
-                  margin_after=self.margin_after, \
-                  padding=self.padding, \
+               "padding={padding}, whitespace={whitespace}>".format(
+                  tag=self.tag,
+                  prefix=self.prefix,
+                  suffix=self.suffix,
+                  display=self.display,
+                  margin_before=self.margin_before,
+                  margin_after=self.margin_after,
+                  padding=self.padding,
                   whitespace=self.whitespace)
+
 
 class CssParse(object):
     '''
@@ -53,7 +56,7 @@ class CssParse(object):
     `HtmlElement.display=Display.none`.
     '''
     # used to separate value and unit from each other
-    RE_UNIT = re_compile(r'([0-9\.]+)(\w+)')
+    RE_UNIT = re_compile(r'([\-0-9\.]+)(\w+)')
 
     @staticmethod
     def get_style_attribute(style_attribute, html_element):
@@ -71,19 +74,19 @@ class CssParse(object):
         '''
         custome_html_element = html_element.clone()
         for style_directive in style_attribute.lower().split(';'):
-            if not ':' in style_directive:
+            if ':' not in style_directive:
                 continue
             key, value = (s.strip() for s in style_directive.split(':', 1))
 
             try:
-                apply_style = getattr(CssParse, "_attr_" + \
-                        key.replace('-webkit-', '').replace("-", "_"))
+                apply_style = getattr(CssParse, "_attr_" +
+                                      key.replace('-webkit-', '')
+                                      .replace("-", "_"))
                 apply_style(value, custome_html_element)
             except AttributeError:
                 pass
 
         return custome_html_element
-
 
     @staticmethod
     def _get_em(length):
@@ -130,7 +133,6 @@ class CssParse(object):
     @staticmethod
     def _attr_padding_left(value, html_element):
         html_element.padding = CssParse._get_em(value)
-
 
     # register aliases
     _attr_margin_before = _attr_margin_top
