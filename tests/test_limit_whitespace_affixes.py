4f18ec8740614af9bdc56109206d735923a64bba
Albert Weichselbraun <albert@weichselbraun.net>
2020-05-19 17:13:18 +0200

    fix: improved html_element_refinement unit test.

--- tests/test_limit_whitespace_affixes.py
+++ tests/test_limit_whitespace_affixes.py
@@ -6,10 +6,41 @@ Tests different HTML to text conversion options.
 
 from inscriptis import get_text
 from inscriptis.css_profiles import RELAXED_CSS_PROFILE
+from inscriptis.html_properties import Display, WhiteSpace
 from inscriptis.model.config import ParserConfig
+from inscriptis.model.css import HtmlElement
 
 
-def test_display_links():
+def test_html_element_refinement():
+    new = HtmlElement('span', display=Display.inline, prefix=' ', suffix=' ',
+                      limit_whitespace_affixes=True)
+    pre = HtmlElement('pre', display=Display.block, whitespace=WhiteSpace.pre)
+    code = HtmlElement('code')
+
+    # refinement with pre and whitespaces
+    refined = pre.get_refined_html_element(new)
+    assert refined.prefix == ''
+    assert refined.suffix == ''
+
+    # refinement with code and whitespaces
+    refined = code.get_refined_html_element(new)
+    assert refined.prefix == ' '
+    assert refined.suffix == ' '
+
+    # refinement with pre and non-whitespaces
+    new.prefix = ' 1. '
+    new.suffix = '<'
+    refined = pre.get_refined_html_element(new)
+    assert refined.prefix == ' 1. '
+    assert refined.suffix == '<'
+
+    # refinement with code and non-whitespaces
+    refined = code.get_refined_html_element(new)
+    assert refined.prefix == ' 1. '
+    assert refined.suffix == '<'
+
+
+def test_limit_whitespace_affixes():
     html = '''<html>
                  <body>
                    hallo<span>echo</span>
