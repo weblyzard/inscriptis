e5eb9ec8a7d266f6ffda8edeb6b2710ba7fa3d2d
Albert Weichselbraun <albert@weichselbraun.net>
2021-06-03 08:39:48 +0200

    add: unittest.

---
+++ tests/test_block.py
@@ -0,0 +1,65 @@
+"""
+Test cases for the Block class.
+"""
+from inscriptis.model.block import Block
+from inscriptis.model.prefix import Prefix
+
+
+def test_merge_normal_text_collapsable_whitespaces():
+    """
+    test cases where the block has collapsable whitespaces
+    """
+    b = Block(0, Prefix())
+    b.merge_normal_text("Hallo")
+    assert b.content == 'Hallo'
+    assert not b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.merge_normal_text(" Hallo ")
+    assert b.content == 'Hallo '
+    assert b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.merge_normal_text('')
+    assert b.content == ''
+    assert b.collapsable_whitespace
+
+    b.merge_normal_text(' ')
+    assert b.content == ''
+    assert b.collapsable_whitespace
+
+    b.merge_normal_text('  ')
+    assert b.content == ''
+    assert b.collapsable_whitespace
+
+
+def test_merge_normal_non_collapsable_whitespaces():
+    b = Block(0, Prefix())
+    b.collapsable_whitespace = False
+    b.merge_normal_text("Hallo")
+    assert b.content == 'Hallo'
+    assert not b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.collapsable_whitespace = False
+    b.merge_normal_text(" Hallo ")
+    assert b.content == ' Hallo '
+    assert b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.collapsable_whitespace = False
+    b.merge_normal_text('')
+    assert b.content == ''
+    assert not b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.collapsable_whitespace = False
+    b.merge_normal_text(' ')
+    assert b.content == ' '
+    assert b.collapsable_whitespace
+
+    b = Block(0, Prefix())
+    b.collapsable_whitespace = False
+    b.merge_normal_text('  ')
+    assert b.content == ' '
+    assert b.collapsable_whitespace
