d26ef2342cdeea5b0089db7ed9918b1400244d0d
Albert Weichselbraun <albert@weichselbraun.net>
2021-06-12 11:33:00 +0200

    add: annotation unittests now also cover the relaxed indentation profiles

--- tests/test_html_snippets_annotations.py
+++ tests/test_html_snippets_annotations.py
@@ -6,12 +6,21 @@ This test case verifies that annotation are correctly computed.
 import os
 from json import load
 from glob import glob
+from typing import List
 
 from inscriptis.engine import get_annotated_text
 from inscriptis.css_profiles import CSS_PROFILES
 from inscriptis.model.config import ParserConfig
 
 TESTCASE_PATTERN = os.path.join(os.path.dirname(__file__), 'html/*.json')
+
+
+def assert_equal_ignoring_whitespace(reference: List[str],
+                                     converted: List[str]) -> bool:
+    for (ref_tag, ref_str), (conv_tag, conv_str) in zip(reference, converted):
+
+        assert ref_tag == conv_tag
+        assert ''.join(ref_str.split()) == ''.join(conv_str.split())
 
 
 def test_html_annotations(filter_str=''):
@@ -26,17 +35,25 @@ def test_html_annotations(filter_str=''):
             print(f.name)
             html = '<html><body>{}</body></html>'.format(f.read())
 
-        result = get_annotated_text(html, ParserConfig(
-            css=CSS_PROFILES['strict'],
-            annotation_rules=reference['annotation_rules']))
-        converted = [[a[2], result['text'][a[0]:a[1]]]
-                     for a in result['label']]
+        for indentation_strategy in ('strict', 'relaxed'):
+            result = get_annotated_text(html, ParserConfig(
+                css=CSS_PROFILES[indentation_strategy],
+                annotation_rules=reference['annotation_rules']))
 
-        print("Reference:")
-        print(reference['result'])
-        print("\nConverted:")
-        print(converted)
-        assert reference['result'] == converted
+            converted = [[a[2], result['text'][a[0]:a[1]]]
+                        for a in result['label']]
+
+            if reference['result'] != converted:
+                print("Reference:")
+                print(reference['result'])
+                print("\nConverted (indentation strategy: {})".format(indentation_strategy))
+                print(converted)
+
+            if indentation_strategy == 'strict':
+                assert reference['result'] == converted
+            else:
+                assert_equal_ignoring_whitespace(reference['result'],
+                                                 converted)
 
 
 if __name__ == '__main__':
