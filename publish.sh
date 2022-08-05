cef329aed9cfea60285fb71c213090b5f142efab
Albert Weichselbraun <albert@weichselbraun.net>
2022-08-05 09:13:07 +0200

    chg: updated publish.sh

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@
 # - https://packaging.python.org/guides/making-a-pypi-friendly-readme/
 
 VERSION=$(grep -Po "\b__version__ = '\K[^']+" src/inscriptis/metadata.py)
-IMAGE_NAME=inscriptis-web-service
+IMAGE_NAME=inscriptis
 
 case "$1" in
 	python)
@@ -24,13 +24,14 @@ case "$1" in
 		;;
 	docker)
 		echo "Publishing ${IMAGE_NAME} in version ${VERSION}"
-		docker login docker.pkg.github.com -u AlbertWeichselbraun --password-stdin < ../github-token.txt
+		docker login ghcr.io -u AlbertWeichselbraun --password-stdin < ../github-token.txt
 		docker build -t ${IMAGE_NAME}:${VERSION} .
 
 		# Step 2: Tag
-		docker tag ${IMAGE_NAME}:${VERSION} docker.pkg.github.com/weblyzard/inscriptis/${IMAGE_NAME}:${VERSION}
+		docker tag ${IMAGE_NAME}:${VERSION} ghcr.io/weblyzard/${IMAGE_NAME}:${VERSION}
+		docker tag ${IMAGE_NAME}:${VERSION} ghcr.io/weblyzard/${IMAGE_NAME}:latest
 
 		# Step 3: Publish
-		docker push docker.pkg.github.com/weblyzard/inscriptis/${IMAGE_NAME}:${VERSION}
+		docker push ghcr.io/weblyzard/${IMAGE_NAME}:${VERSION}
 		;;
 esac
