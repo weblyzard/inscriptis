b0c5882d737d9a2cf97fb12c5ba01e7234c0a124
Philipp Kuntschik <9104613+PhilippKuntschik@users.noreply.github.com>
2022-08-08 09:02:12 +0200

    init empty gh-pages

--- src/inscriptis/model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The model used for HTML rendering.
-
-- :mod:`inscriptis.model.canvas`: classes required for rendering parts of
-    the HTML page.
-- :mod:`inscriptis.model.css`: classes required for the CSS support.
-- :mod:`inscriptis.model.table`: support for rendering HTML tables.
-"""
--- tests/html/br.txt
+++
@@ -1,2 +0,0 @@
-First line
-second line
