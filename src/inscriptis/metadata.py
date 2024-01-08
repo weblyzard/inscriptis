74161ea277c75a29ba6970d12d15b73e6d0f6050
Albert Weichselbraun <albert@weichselbraun.net>
2024-01-08 16:03:20 +0100

    chg: add python 3.12 support.

--- src/inscriptis/metadata.py
+++ src/inscriptis/metadata.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Albert Weichselbraun, Fabian Odoni'
 __author_email__ = 'albert.weichselbraun@fhgr.ch, fabian.odoni@fhgr.ch'
-__copyright__ = '2016-2023 Albert Weichselbraun, Fabian Odoni'
+__copyright__ = '2016-2024 Albert Weichselbraun, Fabian Odoni'
 __license__ = 'Apache 2.0'
-__version__ = '2.3.2'
+__version__ = '2.3.3'
