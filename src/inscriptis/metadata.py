023005c262e0b0de6aaba1b600597f585299876e
Albert Weichselbraun <albert@weichselbraun.net>
2025-01-07 15:39:49 +0100

    chg: adapted year.

--- src/inscriptis/metadata.py
+++ src/inscriptis/metadata.py
@@ -8,7 +8,7 @@ __author__ = "Albert Weichselbraun, Fabian Odoni"
 __author_email__ = "albert.weichselbraun@fhgr.ch, fabian.odoni@fhgr.ch"
 __copyright__ = (
     f"{metadata.metadata(PACKAGE)['Name']} "
-    + f"{metadata.metadata(PACKAGE)['Version']} © 2016-2023 {__author__}"
+    + f"{metadata.metadata(PACKAGE)['Version']} © 2016-2025 {__author__}"
 )
 __license__ = metadata.metadata(PACKAGE)["License"]
 __version__ = metadata.metadata(PACKAGE)["Version"]
